37ec5464c4b154a02cb812796df13f36326c52d5
David Walther <walthdav@gmail.com>
2022-05-13 21:44:06 +0200

    create uninstall script

---
+++ scripts/deployment/uninstallAllPackages.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ALIAS=$1
+echo ""
+echo "----------------------"
+echo "uninstall mapping pkg"
+echo "----------------------"
+sfdx force:package:uninstall --targetusername "$ALIAS" --package="mapping@0.1.0-3" --wait 60
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
+
+echo ""
+echo "--------------------"
+echo "uninstall core pkg"
+echo "-------------------"
+sfdx force:package:uninstall --targetusername "$ALIAS" --package="core@0.1.0-2" --wait 60
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    exit $retVal
+fi
