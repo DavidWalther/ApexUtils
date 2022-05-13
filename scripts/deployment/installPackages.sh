89c5e8b143520f884e62ff4f23d7548af1430110
David Walther <walthdav@gmail.com>
2022-05-13 13:41:53 +0200

    modify install script install all packages

--- scripts/deployment/installPackages.sh
+++ scripts/deployment/installPackages.sh
@@ -3,18 +3,16 @@
 ALIAS=$1
 echo "Installing core pkg"
 echo "-------------------"
-sfdx force:package:install -u "$ALIAS" --package="core@0.1.0-1" --wait 30
+sfdx force:package:install -u "$ALIAS" --package="core@0.1.0-1" --wait 60
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    echo "Error"
+    exit $retVal
 fi
-exit $retVal
 
 echo "Installing mapping pkg"
 echo "----------------------"
-sfdx force:package:install -u "$ALIAS" --package="mapping@0.1.0-3" --wait 30
+sfdx force:package:install -u "$ALIAS" --package="mapping@0.1.0-3" --wait 60
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    echo "Error"
+  exit $retVal
 fi
-exit $retVal
