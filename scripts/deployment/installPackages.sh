38cdf71cd6e94df097c920bf97c21660f449e13f
David Walther <walthdav@gmail.com>
2022-05-25 07:55:31 +0200

    promotion new versions

--- scripts/deployment/installPackages.sh
+++ scripts/deployment/installPackages.sh
@@ -5,7 +5,7 @@ echo ""
 echo "--------------------"
 echo "Installing ApexUtilities-Core"
 echo "-------------------"
-sfdx force:package:install --targetusername "$ALIAS" --package="ApexUtilities-Core@1.0.0-2" --wait 60
+sfdx force:package:install --targetusername "$ALIAS" --package="ApexUtilities-Core@1.1.0-1" --wait 60
 retVal=$?
 if [ $retVal -ne 0 ]; then
     exit $retVal
@@ -15,7 +15,9 @@ echo ""
 echo "----------------------"
 echo "Installing ApexUtilities-Mapping"
 echo "----------------------"
-sfdx force:package:install --targetusername "$ALIAS" --package="ApexUtilities-Mapping@1.0.0-2" --wait 60
+#sfdx force:package:install --targetusername "$ALIAS" --package="ApexUtilities-Mapping@1.1.0-1" --wait 60
+sfdx force:package:install --targetusername "$ALIAS" --package="04t2o000000yUVtAAM" --wait 60
+
 retVal=$?
 if [ $retVal -ne 0 ]; then
   exit $retVal
--- scripts/deployment/uninstallAllPackages.sh
+++ scripts/deployment/uninstallAllPackages.sh
@@ -5,7 +5,7 @@ echo ""
 echo "----------------------"
 echo "uninstall ApexUtilities-Mapping"
 echo "----------------------"
-sfdx force:package:uninstall --targetusername "$ALIAS" --package="ApexUtilities-Mapping@1.0.0-2" --wait 60
+sfdx force:package:uninstall --targetusername "$ALIAS" --package="ApexUtilities-Mapping@1.1.0-1" --wait 60
 retVal=$?
 if [ $retVal -ne 0 ]; then
   exit $retVal
@@ -15,7 +15,7 @@ echo ""
 echo "--------------------"
 echo "uninstall ApexUtilities-Core"
 echo "-------------------"
-sfdx force:package:uninstall --targetusername "$ALIAS" --package="ApexUtilities-Core@1.0.0-2" --wait 60
+sfdx force:package:uninstall --targetusername "$ALIAS" --package="ApexUtilities-Core@1.1.0-1" --wait 60
 retVal=$?
 if [ $retVal -ne 0 ]; then
     exit $retVal
