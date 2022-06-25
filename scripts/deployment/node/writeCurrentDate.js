4f2a0630114286cf18d42a6a128b77d9aad06d5b
David Walther <walthdav@gmail.com>
2022-06-25 21:02:17 +0200

    - print current date to terminal

--- scripts/deployment/node/writeCurrentDate.js
+++ scripts/deployment/node/writeCurrentDate.js
@@ -1,3 +1,12 @@
+const { Console } = require('console');
 const fs = require('fs');
 
 console.log('i am a node script');
+
+let currentDate = new Date();
+const dd = String(currentDate.getDate()).padStart(2, '0');
+const mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyy = currentDate.getFullYear();
+
+currentDate = mm + '/' + dd + '/' + yyyy;
+console.log(currentDate);
