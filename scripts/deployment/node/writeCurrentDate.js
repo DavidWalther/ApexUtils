6d05bddbf65deacacf893090c2c57e50d6492f99
David Walther <walthdav@gmail.com>
2022-06-25 21:16:58 +0200

    - save date string in checksum file

--- scripts/deployment/node/writeCurrentDate.js
+++ scripts/deployment/node/writeCurrentDate.js
@@ -2,10 +2,24 @@ const fs = require('fs');
 
 console.log('i am a node script');
 
-let currentDate = new Date();
-const dd = String(currentDate.getDate()).padStart(2, '0');
-const mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
-const yyyy = currentDate.getFullYear();
+const currentDateString = getCurrentDateString();
+console.log('date for caching checksum:' + currentDateString);
 
-currentDate = yyyy + '-' + mm + '-' + dd;
-console.log(currentDate);
+
+fs.writeFile("checksum.txt", currentDateString, 'utf8', function (err) {
+  if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+  }
+
+  console.log("checksum written");
+});
+
+function getCurrentDateString() {
+  let currentDate = new Date();
+  const dd = String(currentDate.getDate()).padStart(2, '0');
+  const mm = String(currentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = currentDate.getFullYear();
+  
+  return yyyy + '-' + mm + '-' + dd;
+}
