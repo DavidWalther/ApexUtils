c19e4d01389ea8ba12a6fbc1e2d03f310dc3c5f3
David Walther <walthdav@gmail.com>
2022-11-21 05:35:30 +0100

    check for authfile in script

--- scripts/deployment/createNewScratchOrg.sh
+++ scripts/deployment/createNewScratchOrg.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 ALIAS=$1
-echo "Creating Org"
-echo "------------"
-sfdx force:org:create --setalias $ALIAS --definitionfile ./config/project-scratch-def.json --durationdays 1
+AUTHFILE=$2
+
+if [ ! -f "$AUTHFILE" ]; then
+  echo "$AUTHFILE does not exist."
+  
+  echo "Creating Org"
+  echo "------------"
+  sfdx force:org:create --setalias $ALIAS --definitionfile ./config/project-scratch-def.json --durationdays 1
+fi
