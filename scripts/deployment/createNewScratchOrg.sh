ef52e3b343a43e8fed60bdeee30d25fe40d08443
David Walther <walthdav@gmail.com>
2022-11-23 06:02:12 +0100

    revert changes to authfile in creation

--- scripts/deployment/createNewScratchOrg.sh
+++ scripts/deployment/createNewScratchOrg.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 ALIAS=$1
-AUTHFILE=$2
+CREDFILE=$2
 
-if [ ! -f "$AUTHFILE" ]; then
-  echo "$AUTHFILE does not exist."
+if [ ! -f "$CREDFILE" ]; then
+  echo "$CREDFILE does not exist."
   
   echo "Creating Org"
   echo "------------"
-  sfdx force:org:create --setalias $ALIAS --definitionfile ./config/project-scratch-def.json --durationdays 1
-  sfdx force:org:display --targetusername "$ALIAS" --verbose --json > "$AUTHFILE"
+  sfdx force:org:create --setalias "$ALIAS" --definitionfile ./config/project-scratch-def.json --durationdays 1
+  echo ""
 else
-  echo "$AUTHFILE does exist."
+  echo "$CREDFILE does exist. skip Creation"
+  echo ""
 fi
