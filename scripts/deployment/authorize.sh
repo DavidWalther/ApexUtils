4f06bf22aff33b225e6310bf4a61ee5061f8bed5
David Walther <walthdav@gmail.com>
2022-11-25 21:47:11 +0100

    add comments to authorization script

--- scripts/deployment/authorize.sh
+++ scripts/deployment/authorize.sh
@@ -2,29 +2,24 @@
 
 
 
-KEY_FILE_PATH=$1
+JWT=$1
 CLIENT_ID=$2
+
+# Path to the NON-JSON-file containin org details
 FILEPATH=$3
+
+# Alias for the authorized org
 ALIAS=$4
+
+# optional parameter
 SET_DEFAULTUSERNAME=$5
 
-
-
+#extract instance url from org details
 INSTANCE_URL_LINE=$( cat $FILEPATH | grep "Instance Url" )
 INSTANCE_URL="${INSTANCE_URL_LINE/"Instance Url"/}"
 
+#extract username from org details
 USERNAME_LINE=$( cat $FILEPATH | grep "Username" )
 USERNAME="${USERNAME_LINE/"Username"/}"
 
-echo $INSTANCE_URL
-echo $USERNAME
 
-
-
-
-      # clientid: consumerKey,
-      # username: scratchorg.username,
-      # jwtkeyfile: serverKeyPath,
-      # instanceurl: scratchorg.instanceUrl,
-      # setalias: alias,
-      # setdefaultusername:true
