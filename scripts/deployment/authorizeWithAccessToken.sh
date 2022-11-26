bbf6c69a14bc5543dff4b22200f3f54568f761fb
DavidWalther <walthdav@gmail.com>
2022-11-26 12:19:27 +0000

    remove 'echos' from auth script

--- scripts/deployment/authorizeWithAccessToken.sh
+++ scripts/deployment/authorizeWithAccessToken.sh
@@ -7,20 +7,15 @@
 FILEPATH=$1
 ALIAS=$2
 
-echo $FILEPATH
 
 INSTANCE_URL_LINE=$( cat $FILEPATH | grep "Instance Url" )
-echo $INSTANCE_URL_LINE
 INSTANCE_URL="${INSTANCE_URL_LINE/"Instance Url"/}"
-echo $INSTANCE_URL
 
 ACCESS_TOKEN_LINE=$( cat $FILEPATH | grep "Access Token" )
-echo $ACCESS_TOKEN_LINE
 ACCESS_TOKEN="${ACCESS_TOKEN_LINE/"Access Token"/}"
-echo $ACCESS_TOKEN
 
 export SFDX_ACCESS_TOKEN=$ACCESS_TOKEN
-echo $SFDX_ACCESS_TOKEN
+
 
 
 # ==========================
