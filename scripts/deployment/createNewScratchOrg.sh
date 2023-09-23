0fafa364f5c784c764e49af12a17647886944ed5
DavidWalther <walthdav@gmail.com>
2023-09-23 12:18:04 +0000

    cat credentials after creation

--- scripts/deployment/createNewScratchOrg.sh
+++ scripts/deployment/createNewScratchOrg.sh
@@ -11,6 +11,7 @@ if [ ! -f "$CREDFILE" ]; then
   sfdx force:org:create --setalias "$ALIAS" --definitionfile ./config/project-scratch-def.json --durationdays 1
   sfdx force:org:display --targetusername "$ALIAS" --verbose > "$CREDFILE"
   echo ""
+  cat "$CREDFILE"
 else
   echo "$CREDFILE does exist. skip Creation"
   echo ""
